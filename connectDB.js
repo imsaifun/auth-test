082f69b045b1cc79360405ff10c2fb2dfe94db19
imsaifun <imsaifun@gmail.com>
2022-07-14 20:54:26 +0600

    working

--- connectDB.js
+++ connectDB.js
@@ -1,15 +1,42 @@
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 
-const connectDB = async () => {
-  if (mongoose.connection.readyState >= 1) {
-    return
-  }
-  mongoose
-    .connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((con) => console.log("connected to DB"))
+// const connectDB = async () => {
+//   if (mongoose.connection.readyState >= 1) {
+//     return
+//   }
+//   mongoose
+//     .connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     })
+//     .then((con) => console.log("connected to DB"))
+// }
+
+// export default connectDB
+
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+if (!MONGODB_URI) {
+  throw new Error(
+    'please define MONGODB_URI in the env.local file'
+  );
 }
 
-export default connectDB
+let cached = global.mongoose;
+if (!cached) { cached = global.mongoose = { conn: null, promise: null }; }
+
+export default async function connectDB() {
+  if (cached.conn) { return cached.conn; };
+
+  if (!cached.promise) {
+    const options = { bufferCommands: false, };
+
+    cached.promise = mongoose.connect(MONGODB_URI, options)
+      .then(mongoose => mongoose);
+  }
+  cached.conn = await cached.promise;
+  console.log('connected to mdb')
+  return cached.conn;
+}
+
