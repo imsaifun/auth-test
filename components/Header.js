8a805a368ff7ee6d5c3d038d75318ad957336c10
imsaifun <imsaifun@gmail.com>
2022-07-15 08:46:41 +0600

    working

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,4 @@
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
-import AppBar from "@mui/material/AppBar"
-import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
-import IconButton from "@mui/material/IconButton"
-import Toolbar from "@mui/material/Toolbar"
-import Typography from "@mui/material/Typography"
 import cookie from "js-cookie"
 import { signOut, useSession } from "next-auth/react"
 import Link from "next/link"
@@ -28,8 +22,8 @@ export default function ButtonAppBar() {
   const user = cookies?.user
     ? JSON.parse(cookies.user)
     : session?.user
-    ? session?.user
-    : ""
+      ? session?.user
+      : ""
 
   // console.log(userState)
   useEffect(() => {
@@ -60,51 +54,40 @@ export default function ButtonAppBar() {
   }
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            // sx={{ flexGrow: 1 }}
-          >
-            <Link href="/">
-              <LockOutlinedIcon />
-            </Link>
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            AuthApp
-          </Typography>
-          <Link href="/user/profile" passHref>
-            <Button color="inherit">{userState && userState.name}</Button>
-          </Link>
-          <Link href="/user/author" passHref>
-            <Button color="inherit">Author</Button>
-          </Link>
+    <>
 
-          <Box sx={{ ml: 2 }}>
-            {userState ? (
-              <>
-                <Button color="inherit" onClick={logoutHandler}>
-                  Logout
-                </Button>
-              </>
-            ) : (
-              <>
-                <Link href="/user/login">
-                  <Button color="inherit">Login</Button>
-                </Link>
-                <Link href="/user/register">
-                  <Button color="inherit">Register</Button>
-                </Link>
-              </>
-            )}
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Link href="/"passHref>
+        <h3 >
+          AuthApp
+        </h3>
+      </Link>
+      <Link href="/user/profile" passHref>
+        <a className="btn btn-primary">{userState && userState.name}</a>
+      </Link>
+      {/* <Link href="/user/author" passHref>
+        <button className="btn btn-primary">Author</button>
+      </Link> */}
+
+      <div>
+        {userState ? (
+          <>
+            <button className="btn btn-primary" onClick={logoutHandler}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link href="/user/login"passHref>
+              <a className="btn btn-primary">Login</a>
+            </Link>
+            <br />
+            <Link href="/user/register"passHref>
+              <a className="btn btn-primary">Register</a>
+            </Link>
+          </>
+        )}
+      </div>
+    </>
+
   )
 }
